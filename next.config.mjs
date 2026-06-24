9b714246d6c187fc6acc6693c3f1513878f889e0
bora.gungor <Bora.Gungor@vodafone.com>
2026-06-24 17:01:46 +0300

    feat: dockerize app with /redis-gui basePath for testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
